1ec0e434b65f199dd9a7d37a73b75606e37bf88f
BCITKevin <kso51@my.bcit.ca>
2024-03-31 19:24:24 -0700

    finished task1

---
+++ src/graphQL.js
@@ -0,0 +1,106 @@
+"use strict";
+
+import { randomUUID } from "crypto";
+
+export const schema = `
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    tag: Tag!
+  }
+
+  type Tag {
+    id: ID!
+    name: String!
+  }
+
+  input PostCreate {
+    title: String!
+    content: String!
+    tagId: ID!
+  }
+
+  input PostUpdate {
+    id: ID!
+    title: String
+    content: String
+  }
+  
+  input PostDelete {
+    id: ID!
+  }
+
+  input TagCreate {
+    name: String!
+  }
+
+  type Query {
+    getTags: [Tag!]!
+    getPosts: [Post!]!
+    getPost(id: ID!): Post
+    getPostByTag(tagId: ID!): [Post!]!
+  }
+
+  type Mutation {
+    createPost(newPost: PostCreate!): Post!
+    deletePost(deletePost: PostDelete!): Post
+    updatePost(updatePost: PostUpdate!): Post!
+    createTag(newTag: TagCreate!): Tag!
+  }
+`;
+
+export const resolvers = {
+  Query: {
+    getPosts: (_parent, args, { app }) => {
+      return app.db.posts;
+    },
+    getPost: (_parent, args, { app }) => {
+      const { id } = args;
+      return app.db.posts.find((post) => post.id === id);
+    },
+  },
+  Mutation: {
+    createPost: (_parent, { newPost }, { app }) => {
+      const { title, content } = newPost;
+
+      const post = {
+        id: randomUUID(),
+        title,
+        content,
+      };
+      app.db.posts.push(post);
+      return post;
+    },
+    deletePost: (_parent, { deletePost }, { app }) => {
+      const { id } = deletePost;
+      const postDelete = app.db.posts.filter(post => post.id !== id);
+
+      app.db.posts = postDelete
+    },
+    updatePost: (_parent, { updatePost }, { app }) => {
+      const { id, title, content } = updatePost;
+      const post = app.db.posts.find(post => post.id === id);
+
+      if (title) {
+        post.title = title;
+      }
+      if (content) {
+        post.content = content;
+      }
+
+      return post;
+    },
+    createTag: (_parent, { newTag }, { app }) => {
+      const { name } = newTag;
+
+      const tag = {
+        id: randomUUID(),
+        name: name,
+      };
+      app.db.posts.push(tag);
+    }
+  },
+};
+
+export const loaders = {};
